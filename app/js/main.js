fcc5a5227be12741614f9197b487af26ad7f1a9e
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-23 17:30:43 +0500

    added filter for sections: products and new

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,14 @@ $(function () {
 		autoplay: true,
 		autoplaySpeed: 4000
 	});
+	const mixer1 = mixitup('.products__items', {
+		selectors: {
+			control: '.filter-1'
+		}
+	});
+	const mixer2 = mixitup('.new__items', {
+		selectors: {
+			control: '.filter-2'
+		}
+	});
 });
