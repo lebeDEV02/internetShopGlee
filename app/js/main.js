499aadaaeee7f0c99b463fb5f9a2f4800374b924
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-28 20:02:27 +0500

    added product list 1 page

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,22 @@ $(function () {
 		autoplay: true,
 		autoplaySpeed: 4000
 	});
+	$('.shop-filters__price-input').ionRangeSlider({
+		type: "double",
+		prefix: "$",
+		onStart: function (data) {
+			$('.shop-filter__price-from').text(data.from);
+			$('.shop-filter__price-to').text(data.to);
+		},
+		onChange: function (data) {
+			$('.shop-filter__price-from').text(data.from);
+			$('.shop-filter__price-to').text(data.to);
+		}
+	});
+	$('.shop-filters__recent-stars').rateYo({
+		starWidth: '12px',
+		readOnly: true,
+	})
 	const mixer1 = mixitup('.products__items', {
 		selectors: {
 			control: '.filter-1'
