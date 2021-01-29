c061462efc1a36c782975dacb4adb9a5f8db37d1
ledeDEV02 <vladimirlebedev00010@gmail.com>
2021-01-29 11:57:00 +0500

    added shop-2 page

--- app/js/main.js
+++ app/js/main.js
@@ -6,22 +6,31 @@ $(function () {
 		autoplay: true,
 		autoplaySpeed: 4000
 	});
+	function my_prettify(n) {
+		return n.toFixed(2);
+	}
 	$('.shop-filters__price-input').ionRangeSlider({
 		type: "double",
 		prefix: "$",
+		prettify: my_prettify,
+		step: 0.01,
 		onStart: function (data) {
-			$('.shop-filter__price-from').text(data.from);
-			$('.shop-filter__price-to').text(data.to);
+			$('.shop-filter__price-from').text(data.from_pretty);
+			$('.shop-filter__price-to').text(data.to_pretty);
 		},
 		onChange: function (data) {
-			$('.shop-filter__price-from').text(data.from);
-			$('.shop-filter__price-to').text(data.to);
+			$('.shop-filter__price-from').text(data.from_pretty);
+			$('.shop-filter__price-to').text(data.to_pretty);
 		}
 	});
 	$('.shop-filters__recent-stars').rateYo({
 		starWidth: '12px',
 		readOnly: true,
-	})
+	});
+	$('.shop-list__stars').rateYo({
+		starWidth: '18px',
+		readOnly: true,
+	});
 	const mixer1 = mixitup('.products__items', {
 		selectors: {
 			control: '.filter-1'
@@ -31,5 +40,8 @@ $(function () {
 		selectors: {
 			control: '.filter-2'
 		}
+	});
+	$('.user-nav__link--first').on('click', function () {
+		$('.user-nav__wrapper').toggleClass('active');
 	});
 });
